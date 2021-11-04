d30a4bdf526d395a73ebfc7f322ff5c583fab4a5
Roman <roman.paszkow@gmail.com>
2021-11-04 20:59:18 +0100

    added two tests to TopicControllerTest2

--- src/test/java/com/pashkov/driverapi/app/resource/TopicControllerTest2.java
+++ src/test/java/com/pashkov/driverapi/app/resource/TopicControllerTest2.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,5 +22,17 @@ class TopicControllerTest2 {
     @Test
     public void shouldReturnEntityNotFoundExceptionIfThereIsNoSuchTopic() throws Exception {
         this.mvc.perform(get("/topics/Prędkośćsadasdad")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldReturnStatusOkIfTopicExist() throws Exception {
+        this.mvc.perform(get("/topics/Prędkość")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnCollectionOfTopics() throws Exception {
+        this.mvc.perform(get("/topics"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("{\"topics\":")));
     }
 }
